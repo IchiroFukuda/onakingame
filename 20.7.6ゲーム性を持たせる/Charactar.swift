377e12c8b7e43a65976bcc3ee5fa31e04f4d09a7
IchiroFukuda <ichr0113@gmail.com>
2020-07-24 14:59:34 +0900

    20.7.24

---
+++ 20.7.6ゲーム性を持たせる/Charactar.swift
@@ -0,0 +1,126 @@
+//
+//  Charactar.swift
+//  20.7.6ゲーム性を持たせる
+//
+//  Created by 福田一朗 on 2020/07/18.
+//  Copyright © 2020 福田一朗. All rights reserved.
+//
+
+import Foundation
+
+
+class Character {
+    
+    var name : String
+    var level : Int
+    var actionNo : Int
+    var counter : Int
+    
+    init(name n : String,level l: Int, actionNo a : Int , counter c : Int){
+        self.name = n
+        self.level = l
+        self.actionNo = a
+        self.counter = c
+    }
+}
+class characterCsvData{
+        //プロパティのこと
+        //var sentenceValue : String
+    var image0 : String
+    var comment0 : String
+    var button1 : String
+    var button2 : String
+    var image1 : String
+    var image2 : String
+    var comment1 : String
+    var comment2 : String
+    var point1 : Int
+    var point2 : Int
+    
+        //イニシャライザ。muramuraDataをインスタンス化して利用するとき、let a = muramuraData(sentenceData:b) と宣言すれば、sentenceValueにはbが入る。
+    init(image0 i0:String,comment0 c0:String,button1 b1:String,button2 b2:String,image1 i1 : String ,image2 i2 : String , comment1 c1 :String,comment2 c2 :String,point1 p1:Int ,point2 p2:Int) {
+            image0 = i0
+            comment0 = c0
+            button1 = b1
+            button2 = b2
+            image1 = i1
+            image2 = i2
+        comment1 = c1
+        comment2 = c2
+        point1 = p1
+        point2 = p2
+        }
+    }
+class characterCsvDM{
+        
+        static let sharedInstance = characterCsvDM()
+        //空の配列。型だけ指定する。
+    var objectData = characterCsvData(image0: "", comment0: "", button1: "", button2: "", image1: "", image2: "", comment1: "", comment2: "", point1: 0, point2: 0)
+        // 取り出す文の番号を入れる変数
+        var index: Int = 0
+        
+        var csvLines = [String]()
+    
+        private init() {
+        }
+        
+        func loadData(){
+            csvLines.removeAll()
+            //muramuraLevelは0〜10.kakuは1〜12これが日数によるレベル分け。これに10をかけることで、レベルに応じて10個の文章が変わる。
+            let character = BossViewController.character
+           index = character.level * 100 + character.counter * 10
+           
+            var n = ""
+            switch character.name {
+            case  "ボス猿" : n = "Boss"
+            case  "雌猿" : n = "Boss"
+                case  "仙猿" : n = "Boss"
+                case  "ライフ" : n = "Boss"
+                default : n = "エラー"
+            }
+            
+            //csvファイルを閲覧するためのキーが手に入る。
+            guard let csvFilePath = Bundle.main.path(forResource: n,ofType: "csv") else {
+                print("csvファイルが存在しません")
+                return
+                
+            }
+            //おそらくここでcsvファイルを順番に見ていく指示が書かれている。
+            do {
+                let csvStringData = try String(contentsOfFile: csvFilePath, encoding:String.Encoding.utf8)
+               
+                csvLines = csvStringData.components(separatedBy: .newlines)
+                
+                  //  let sentenceLine = line
+                    
+                    //objectData= sentenceLine つまり、objectDataに文が入る。さらにこれはmuramuraData型に変換される。ここで、muramuraDataクラスのsentenceValueには文が入る。
+                    var point1 = 0
+                    var point2 = 0
+                    guard index + 9 >= 0 && index + 9 < csvLines.count else { return }
+                    if let value1 : Int = Int(csvLines[index + 8]) {point1 = value1}
+                    if let value2 : Int = Int(csvLines[index + 9]) {point2 = value2}
+                
+                    objectData  = characterCsvData(image0: csvLines[index], comment0: csvLines[index + 1], button1: csvLines[index + 2], button2: csvLines[index + 3],image1: csvLines[index + 4], image2: csvLines[index + 5], comment1: csvLines[index + 6], comment2: csvLines[index + 7], point1: point1, point2: point2)
+            
+                    //配列にobjectDataが入る。つまり、読み込んだ文が配列に追加される。こうやってどんどん配列に文が入っていく。
+            //self.csvDataArray.append(objectData)
+                    
+                
+            } catch let error {
+                print("csvファイル読み込みエラーが発生しました:\(error)")
+                return
+            }
+        }
+        //nextDataの戻り値をmuramuraData型で返却。sentenceValueには文が入った状態。この文を取り出すには、muramuraData.sentenceValueでOK
+        
+        func nextData() -> characterCsvData? {
+           
+            let nextData = objectData
+            
+            return nextData
+        }
+        
+        
+        
+    }
+
