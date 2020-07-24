447918d548cd382da826314f0baccbe40a4ce36c
IchiroFukuda <ichr0113@gmail.com>
2020-07-24 14:59:10 +0900

    20.7.24

---
+++ 20.7.6ゲーム性を持たせる/LifeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LifeViewController.swift
+//  20.7.6ゲーム性を持たせる
+//
+//  Created by 福田一朗 on 2020/07/09.
+//  Copyright © 2020 福田一朗. All rights reserved.
+//
+
+import UIKit
+
+class LifeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
